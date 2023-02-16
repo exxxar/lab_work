83d80a1fd06f5fd775dea0f179ff0245d9c786c8
Alex <exxxar@gmail.com>
2023-02-16 19:48:03 +0300

    first commit

---
+++ WindowsFormsApp1/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Car
+    {
+        public class Fuel
+        {
+            public int value { get; set; }
+
+            public Fuel(int fuel = 0)
+            {
+                this.value = fuel;
+            }
+        }
+            
+        Fuel fuel = new Fuel();
+        public Car()
+        {
+
+        }
+
+        public Car(int fuell = 12)
+        {
+            this.fuel.value = fuell;
+        }
+
+        public Car(int fuell = 12, string title = "test")
+        {
+
+        }
+        private void start()
+        {
+
+        }
+
+        private bool start(int fuel = 0)
+        {
+            this.fuel.value--;
+
+            return false;
+        }
+
+        private int start(int fuel = 0, String fuelType = "95")
+        {
+            return 0;
+        }
+    }
+}
---
+++ WindowsFormsApp1/ElectroCar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class ElectroCar:Car
+    {
+        double voltage;
+        public ElectroCar()
+        {
+       
+            
+        }
+
+        public void start(double voltage)
+        {
+            this.start((double)voltage*1000);
+
+           // this.voltage = voltage;
+        }
+    }
+}
